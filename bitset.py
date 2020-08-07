2d05a996f9b7d7dd3f17be5e49a8cda46bca2b83
proribone <63295806+proribone@users.noreply.github.com>
2020-08-07 23:25:46 +0900

    Update bitset.py

--- bitset.py
+++ bitset.py
@@ -20,3 +20,27 @@ class bitset:
 
     def isin(self,x):
         return self.b>>x&1
+    
+class M_bitset:
+    def __init__(self,minimum):
+        self.b=0
+        self.pcnt=0
+        self.minimum=abs(minimum)
+        
+    def merge(self,x,y):
+        self.b=x.b|y.b
+        self.pcnt=bin(self.b).count('1')
+        
+    def add(self,x):
+        self.b|=1<<(x+self.minimum)
+        self.pcnt+=1
+        
+    def remove(self,x):
+        self.b^=1<<(x+self.minimum)
+        self.pcnt-=1
+
+    def popcount(self):
+        return self.pcnt
+
+    def isin(self,x):
+        return self.b>>(x+self.minimum)&1
